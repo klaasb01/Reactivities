825a2e34687971f584ec2564bf42883cb4eb1f6d
Brian Klaassens <10063026@bd.com>
2019-12-13 16:19:57 -0500

    No message

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,13 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+
+                } );
+            });
 
             services.AddControllers();
         }
@@ -44,14 +51,15 @@ namespace API
 
             //app.UseHttpsRedirection();
 
-            app.UseRouting();
+           // app.UseRouting();
 
-            app.UseAuthorization();
-
+            //app.UseAuthorization();
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
